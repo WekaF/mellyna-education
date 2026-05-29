d82ae16a3974f839819cad0e44b9a569a17b4546
Wahyu Febryanto <wahyukf01@gmail.com>
2026-05-29 10:17:55 +0700

    fix: allow user zoom (maximumScale 5), add icon-180 to manifest, add generate-icons script

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const viewport: Viewport = {
   themeColor: '#6366f1',
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
+  maximumScale: 5,
 }
 
 export const metadata: Metadata = {
